63e4e517ba62a0b3c76cf9886ba997c4ac233989
Uliana Dzyoba <ulianadzyoba@gmail.com>
2022-07-18 23:30:55 +0300

    migrating to postgresql

--- backend/delivery/deliveryproject/settings.py
+++ backend/delivery/deliveryproject/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'deliveryproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deliveryapp',
+        'USER': 'postgres',
+        'PASSWORD': '28012001',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
