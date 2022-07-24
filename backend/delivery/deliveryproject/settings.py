06f7635cd6bcdd35fdc73ebce123ab9a55d5844c
Uliana Dzyoba <ulianadzyoba@gmail.com>
2022-07-24 22:28:21 +0300

    testing authentication

--- backend/delivery/deliveryproject/settings.py
+++ backend/delivery/deliveryproject/settings.py
@@ -55,7 +55,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'BLACKLIST_AFTER_ROTATION': False,
 }
--- backend/delivery/orders/views.py
+++ backend/delivery/orders/views.py
@@ -1,8 +1,15 @@
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework import generics, status
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, MenuItem
+from .serializers import OrderSerializer, MenuItemSerializer
 
 # Create your views here.
 class OrderListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Order.objects.all().order_by('-delivery_at')
     serializer_class = OrderSerializer
+
+
+class MenuItemListView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all().order_by('name')
+    serializer_class = MenuItemSerializer
