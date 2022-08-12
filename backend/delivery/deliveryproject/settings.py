f575f85781ae2646a4048a69452a31598b5986cf
Uliana Dzyoba <ulianadzyoba@gmail.com>
2022-08-12 20:04:39 +0300

    Update settings.py

--- backend/delivery/deliveryproject/settings.py
+++ backend/delivery/deliveryproject/settings.py
@@ -110,16 +110,16 @@ WSGI_APPLICATION = 'deliveryproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'deliveryapp',
-        'USER': 'postgres',
-        'PASSWORD': '28012001',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'deliveryapp',
+#         'USER': 'postgres',
+#         'PASSWORD': '28012001',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES = {}
 
