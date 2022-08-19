0dc2994ad45864ee48c1982661935eab3c541456
Uliana Dzyoba <ulianadzyoba@gmail.com>
2022-08-19 15:52:18 +0300

    Update urls.py

--- backend/delivery/orders/urls.py
+++ backend/delivery/orders/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('menu/', views.MenuItemPublicListView.as_view(),name='menu'),
-    path('menu/<int:pk>/', views.MenuItemPublicListView.as_view(),name='menu_item'),
+    path('menu/<int:pk>/', views.MenuItemUpdateDeleteView.as_view(),name='menu_item'),
     path('orders/',views.OrderListCreateView.as_view(),name='orders'),
     path('orders/<int:pk>/', views.OrderDetailStatusDeleteView.as_view(), name='order'),
     path('users/<int:user_pk>/orders/', views.UserOrdersListCreateView.as_view(), name='user_orders_list'),
