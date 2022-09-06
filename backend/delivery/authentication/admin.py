3681d66d5c85b1fed162c4811bf68559061b3ab3
Uliana Dzyoba <ulianadzyoba@gmail.com>
2022-09-06 15:46:42 +0300

    admin simplified

--- backend/delivery/authentication/admin.py
+++ backend/delivery/authentication/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-
-class CustomUserAdmin(UserAdmin):
-    pass
-
-
 # Register your models here.
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User)
--- backend/delivery/orders/admin.py
+++ backend/delivery/orders/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Order, MenuItem
 
-# Register your models here.
-
 admin.site.register(MenuItem)
 
 
@@ -10,4 +8,3 @@ admin.site.register(MenuItem)
 class OrderAdmin(admin.ModelAdmin):
     list_display=['id', 'customer', 'order_status', 'delivery_at', 'address']
     list_filter=['delivery_at', 'order_status', 'created_at', 'updated_at']
-
