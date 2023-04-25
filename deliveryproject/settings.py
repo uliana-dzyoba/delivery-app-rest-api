849265758eef69c8fd8b1f07619019de4df97a27
Uliana Dzyoba <ulianadzyoba@gmail.com>
2023-04-25 15:49:38 +0300

    Update settings.py

--- deliveryproject/settings.py
+++ deliveryproject/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'deliveryproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)}
+DATABASES = {'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
